eba2927ed5d715764f769e75a45e2e0e6768053f
Ignatius Du Preez <ignatiuspdupreez@gmail.com>
2017-08-19 10:48:43 +0200

    Update 20161202182552_add_cookie_key_to_cart.rb

--- db/migrate/20161202182552_add_cookie_key_to_cart.rb
+++ db/migrate/20161202182552_add_cookie_key_to_cart.rb
@@ -1,4 +1,4 @@
-class AddCookieKeyToCart < ActiveRecord::Migration
+class AddCookieKeyToCart < ActiveRecord::Migration[5.0]
   def change
     add_column :plugins_ecommerce_orders, :visitor_key, :string
   end
