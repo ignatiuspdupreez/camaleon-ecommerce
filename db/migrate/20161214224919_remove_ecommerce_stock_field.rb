4aeffe31dfa9d4964d123b080e85c5aeee70430c
Ignatius Du Preez <ignatiuspdupreez@gmail.com>
2017-08-19 10:49:31 +0200

    Update 20161214224919_remove_ecommerce_stock_field.rb

--- db/migrate/20161214224919_remove_ecommerce_stock_field.rb
+++ db/migrate/20161214224919_remove_ecommerce_stock_field.rb
@@ -1,4 +1,4 @@
-class RemoveEcommerceStockField < ActiveRecord::Migration
+class RemoveEcommerceStockField < ActiveRecord::Migration[5.0]
   def change
     Cama::PostType.where(slug: 'commerce').each do |pt|
       pt.get_field_object('ecommerce_stock').try(:destroy)
